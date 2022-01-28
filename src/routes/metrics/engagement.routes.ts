f496e268bde32f86da62339f21b15d44aa8bec04
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-28 02:00:40 -0300

    Refactor: update engagement route

--- src/routes/metrics/engagement.routes.ts
+++ src/routes/metrics/engagement.routes.ts
@@ -1,39 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 
-import { MetricsModel } from '../../Database/model/Metrics';
-import { IMetrics } from '../../interfaces/Metrics';
 import { getEngagementController } from '../../useCases/metrics/controllers/getEngagement.controller';
+import { updateEngagementController } from '../../useCases/metrics/controllers/updateEngagement.controller';
 
 const engagementRouter = express.Router();
 
 engagementRouter.get('/', getEngagementController);
 
-engagementRouter.put(
-  '/',
-  async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<IMetrics | undefined> => {
-    const engagement: number = req.body;
-    const { vendorName } = res.locals; // I assume the vendor name comes from an authentication middleware.
-
-    try {
-      const updatedMetrics = await MetricsModel.findOneAndUpdate(
-        { vendorName },
-        { engagement, updateDate: Date.now() },
-        {
-          new: true,
-          upsert: true,
-        }
-      );
-
-      res.status(200).json(updatedMetrics);
-    } catch (error) {
-      next(error);
-    }
-    return undefined;
-  }
-);
+engagementRouter.put('/', updateEngagementController);
 
 export default engagementRouter;
