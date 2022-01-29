bda4616de11f339b6e13388136c220dc99856e2b
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-29 04:52:58 -0300

    Chore: Comment explaining storage of tokens

--- src/utils/createAndSetTokens.ts
+++ src/utils/createAndSetTokens.ts
@@ -6,4 +6,5 @@ export const createAndSetTokens = (id: string) => {
   const refreshToken = createRefreshTokenVendorService(id);
   localStorage.setItem('AccessToken', accessToken);
   localStorage.setItem('RefreshToken', JSON.stringify(refreshToken));
+  // I stored it in local storage despite using Httponly and Secure cookies is a more secure way, because in my last project i have some issues reported by an user using Safari to store and erad cookies
 };
